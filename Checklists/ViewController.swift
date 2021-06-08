39818ba063ff88d23424bb5651e31a6c5fac8072
ssujinn <efsonata38@gmail.com>
2021-06-08 13:38:46 +0900

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by RelMac User Exercise2 on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
