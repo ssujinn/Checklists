f4a4b32464c8fca01f6abb27d476cf3e14228340
ssujinn <efsonata38@gmail.com>
2021-06-21 12:23:22 +0900

    Finish style7

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class AddItemViewControllerTableViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +41,23 @@ class AddItemViewControllerTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView,
                             willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    /* Clear Button */
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 
     /*
