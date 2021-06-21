b55c8a1ea9bbb6b1f08e3e65038d048b06aff98f
ssujinn <efsonata38@gmail.com>
2021-06-21 09:57:35 +0900

    finish navigation 4

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -78,4 +78,23 @@ class ChecklistViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }   // End of didSelectRow
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    /* delete Row */
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
 }
